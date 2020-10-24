f0f60c8f4571f0372af6640df685180de10e15ef
viktorsmer <smerdin-omz@yandex.ru>
2020-10-25 00:37:54 +0300

    создание города

--- cities/views.py
+++ cities/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import City
 from .forms import CityForm
 
@@ -13,10 +15,18 @@ def home(request):
 	city = request.POST.get('name')
 #	print(city)
 	cities = City.objects.all()
-	return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
+	return render(request, 'cities/create.html', {'objects_list': cities, 'form': form})
 
 
 class CityDetailView(DetailView):
 	queryset = City.objects.all()
 	context_object_name = 'object'
 	template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+	model = City
+	form_class = CityForm
+	template_name = 'cities/create.html'
+	success_url = reverse_lazy('city:home')
+
